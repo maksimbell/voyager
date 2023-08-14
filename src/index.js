8fc90562e71184fa14ed069d3b6474e0450995cd
Maksim Beliavsky <maksimbell503@gmail.com>
2023-08-14 10:57:28 +0300

    modify index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import {
+    BrowserRouter
+} from 'react-router-dom';
 import "./App.scss";
 
 const el = document.getElementById("app");
